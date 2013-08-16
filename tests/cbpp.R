b13e2ea4d279fcbdae7594e33c25a77079fcdef4
Steve Walker <scwalker@math.mcmaster.ca>
2013-08-16 00:46:33 -0600

    sorry, I see cbpp.R is in radicalrewrite branch

--- tests/cbpp.R
+++
@@ -1,5 +0,0 @@
-library(lme4)
-library(minqa)
-glmod <- glFormula(incidence / size ~ period + (1 | herd), weights = size,
-             family = binomial, data = cbpp)
-# todo: add actual pirls tests here
