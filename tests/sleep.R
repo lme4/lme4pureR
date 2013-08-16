fed167a068c568489f0f22249c563f39369b82e3
Steve Walker <scwalker@math.mcmaster.ca>
2013-08-16 00:34:42 -0600

    run ML in sleep test

--- tests/sleep.R
+++ tests/sleep.R
@@ -1,6 +1,9 @@
 library(lme4)
+library(minqa)
 lmod <- lFormula(Reaction ~ Days + (Days|Subject), sleepstudy)
-bobyqa(c(1, 0, 1), function(theta) pls(lmod,theta,sleepstudy$Reaction),lower=c(0,-Inf,0))$par
+bobyqa(c(1, 0, 1),
+       function(theta) pls(lmod,theta,sleepstudy$Reaction,REML=FALSE),
+       lower=c(0,-Inf,0))$par
 mML <- lmer(Reaction ~ Days + (Days|Subject),
             sleepstudy, REML = FALSE)
 getME(mML, "theta")
