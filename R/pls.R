2eac56b7e170103047bf01640601af3c9ae316ba
Douglas Bates <dmbates@gmail.com>
2013-08-28 16:13:53 -0500

    Bump version number and date.
    1. The creation of the matrices, etc. to represent the model has been modularized.
    2. The sparseX argument has been added to `plsform` but `pls` still needs
       to be set up to handle a sparse `X` (shouldn't be too hard).
    3. A version of `pirls` to avoid the call to glFormula still needs to
       be written

--- R/pls.R
+++ R/pls.R
@@ -1,6 +1,5 @@
 ##' @importMethodsFrom Matrix t %*% crossprod diag tcrossprod solve determinant update
-##' @importFrom Matrix bdiag rBind Diagonal Cholesky
-##' @importFrom minqa bobyqa
+##' @importFrom Matrix bdiag rBind Diagonal Cholesky sparse.model.matrix
 ##' @importFrom lme4 findbars nobars subbars
 NULL
 
@@ -39,32 +38,28 @@ pls <- function(X,y,Zt,Lambdat,thfun,weights,
     WX <- Whalf %*% X
     Wy <- Whalf %*% y
     ZtW <- Zt %*% Whalf
-    local({
-        b <- numeric(q)
-        beta <- numeric(p)
-        cu <- numeric(q)
-        DD <- NULL
+    XtWX <- crossprod(WX)
+    XtWy <- crossprod(WX, Wy)
+    ZtWX <- ZtW %*% WX
+    ZtWy <- ZtW %*% Wy
+    rm(WX,Wy,ZtW)
+    local({                             # mutable values stored in local environment
+        b <- numeric(q)                 # conditional mode of random effects
+        beta <- numeric(p)              # conditional estimate of fixed-effects
+        cu <- numeric(q)                # intermediate solution
+        DD <- XtWX                      # down-dated XtWX
         L <- Cholesky(tcrossprod(Lambdat %*% Zt), LDL = FALSE, Imult=1)
-        Lambdat <- Lambdat
-        mu <- numeric(n)
-        offset <- offset
-        RZX <- matrix(0,nrow=q,ncol=p)
-        thfun <- thfun
-        u <- numeric(q)
-        whalf <- diag(Whalf)
-        X <- X
-        XtWX <- crossprod(WX)
-        XtWy <- crossprod(WX, Wy)
-        Zt <- Zt
-        ZtWX <- ZtW %*% WX
-        ZtWy <- ZtW %*% Wy
+        Lambdat <- Lambdat              # stored here b/c x slot will be updated
+        mu <- numeric(n)                # conditional mean of response
+        RZX <- matrix(0,nrow=q,ncol=p)  # intermediate matrix in solution
+        u <- numeric(q)                 # conditional mode of spherical random effects
         function(theta) {
             Lambdat@x[] <<- thfun(theta)
             L <<- update(L, Lambdat %*% Zt, mult = 1)
-            ## solve system from equation 30
+                                        # solve eqn. 30
             cu[] <<- as.vector(solve(L, solve(L, Lambdat %*% ZtWy, system="P"),
                                      system="L"))
-            ## solve system from eqn. 31
+                                        # solve eqn. 31
             RZX[] <<- as.vector(solve(L, solve(L, Lambdat %*% ZtWX, system="P"),
                                       system="L"))
             ## downdate XtWX and form Cholesky factor (eqn. 32)
@@ -75,21 +70,19 @@ pls <- function(X,y,Zt,Lambdat,thfun,weights,
             u[] <<- as.vector(solve(L, solve(L, cu - RZX %*% beta, system = "Lt"),
                                     system="Pt"))
             b[] <<- as.vector(crossprod(Lambdat,u))
-            ## conditional mean of the response
+                                        # conditional mean of the response
             mu[] <<- as.vector(crossprod(Zt,b) + X %*% beta + offset)
-
-            wtres <- whalf*(y-mu)      # weighted residuals
-            wrss <- sum(wtres^2)       # weighted residual sums of squares
-            pwrss <- wrss + sum(u^2)   # plus penalty
+            wtres <- Whalf*(y-mu)       # weighted residuals
+            pwrss <- sum(wtres^2) + sum(u^2) # penalized, weighted residual sum-of-squares
             fn <- as.numeric(length(mu))
-            res <- 2*determinant(L,logarithm=TRUE)$modulus # log determinants
+            ld <- 2*determinant(L,logarithm=TRUE)$modulus # log determinant
             if (REML) {
-                res <- res + determinant(DD,logarithm=TRUE)$modulus
+                ld <- ld + determinant(DD,logarithm=TRUE)$modulus
                 fn <- fn - length(beta)
             }
-            attributes(res) <- NULL
+            attributes(ld) <- NULL
                                         # profiled deviance or REML criterion
-            res + fn*(1 + log(2*pi*pwrss) - log(fn))
+            ld + fn*(1 + log(2*pi*pwrss) - log(fn))
         }
     })
 }
@@ -106,46 +99,88 @@ pls <- function(X,y,Zt,Lambdat,thfun,weights,
 ##' @param weights prior weights
 ##' @param offset offset
 ##' @param REML calculate REML deviance?
+##' @param sparseX should X, the model matrix for the fixed-effects coefficients be sparse?
 ##' @param ... additional arguments
 ##' @keywords models
 ##'
 ##' @return a function that evaluates the deviance or REML criterion
 ##' @export
-plsform <- function(formula, data, REML=TRUE, weights, offset, ...)  {
+plsform <- function(formula, data, REML=TRUE, weights, offset, sparseX = FALSE, ...)  {
     stopifnot(inherits(formula, "formula"), length(formula) == 3L,
               length(rr <- findbars(formula[[3]])) > 0L)
-    mf <- mc <- mcout <- match.call()
-    m <- match(c("data", "subset", "weights", "na.action", "offset"), names(mf), 0)
-    mf <- mf[c(1, m)]; mf$drop.unused.levels <- TRUE; mf[[1]] <- quote(model.frame)
-    fr.form <- subbars(formula)         # substitute "+" for "|"
-    environment(fr.form) <- environment(formula)
-    mf$formula <- fr.form
-    fr <- eval(mf, parent.frame())      # evaluate the model frame
-    ff <- formula
-    ff[[3]] <- if(is.null(nb <- nobars(ff[[3]]))) 1 else nb
-    mf$formula <- ff
-    fr1 <- eval(mf,parent.frame())
-    trms <- attr(mf, "terms") <- attr(fr1, "terms")
-    ll <- list(X = model.matrix(trms,fr1),
-               y = model.response(fr, type="numeric"),
-               fr = fr, REML = as.logical(REML)[1])
-    n <- length(ll$y)
-    if (!is.null(wts <- model.weights(fr1))) {
-        stopifnot((lwts <- length(wts)) %in% c(1L,n))
-        ll$weights <- rep.int(wts, n %/% lwts)
-    }
-    if (!is.null(off <- model.offset(fr1))) {
-        stopifnot((loff <- length(off)) %in% c(1L,n))
-        ll$weights <- rep.int(off, n %/% loff)
-    }
-    grps <- lapply(rr, function(t) as.factor(eval(t[[3]], fr)))
+    mc <- match.call()
+    fr <- eval(mkMFCall(mc, formula), parent.frame())      # evaluate the model frame
+    fr1 <- eval(mkMFCall(mc, formula, TRUE), parent.frame())
+    trms <- attr(fr, "terms") <- attr(fr1, "terms")
+    c(list(X = if (sparseX) sparse.model.matrix(trms,fr) else model.matrix(trms,fr),
+           y = model.response(fr, type="numeric"),
+           fr = fr, call = mc,
+           REML = as.logical(REML)[1]),
+      if (is.null(wts <- model.weights(fr))) wts else list(weights=wts),
+      if (is.null(off <- model.offset(fr))) off else list(offset=off),
+      mkLambdat(lapply(rr, function(t) as.factor(eval(t[[3]], fr))),
+                lapply(rr, function(t)
+                       model.matrix(eval(substitute( ~ foo, list(foo = t[[2]]))), fr))))
+}
+
+## Create a section of Zt from a sparse matrix of indicators, zt, and
+## a dense model matrix, mm. When mm has a single column, just
+## multiply zt by a diagonal matrix. For multiple columns, rBind the
+## products of zt and a diagonal matrix for each column, then
+## rearrange the order of the rows.
+Zsection <- function(zt,mm) {
+    if ((m <- ncol(mm)) == 1L) return(zt %*% Diagonal(x=mm))
+    ## Row indices.  If m = 2, nrow(zt) = 10 we want the order 1,11,2,12,3,13,...,20
+    rinds <- as.vector(matrix(seq_len(m*nrow(zt)), nrow=m, byrow=TRUE))
+    do.call(rBind,lapply(seq_len(m), function(j) zt %*% Diagonal(x=mm[,j])))[rinds,]
+}
+
+## Create the diagonal block on Lambdat for a random-effects term with
+## nc columns and nl levels.  The value is a list with the starting
+## value of theta for the block, the lower bounds, the block of
+## Lambdat and the function that updates the block given the section
+## of theta for this block.
+Lambdatblock <- function(nc, nl) {
+    if (nc == 1L)
+        return(list(theta = 1,
+                    lower = 0,
+                    Lambdat = Diagonal(x = rep(1, nl)),
+                    updateLambdatx=local({nl <- nl;function(theta) rep.int(theta[1],nl)})))
+    m <- diag(nrow=nc, ncol=nc); ut <- upper.tri(m, diag=TRUE)
+    theta <- m[ut]; m[ut] <- seq_along(theta)
+    Lambdat <- do.call(bdiag, lapply(seq_len(nl), function(i) m))
+    list(theta=theta,
+         lower=ifelse(theta,0,-Inf),
+         Lambdat=Lambdat,
+         updateLambdatx = local({
+             Lind <- Lambdat@x
+             function(theta) theta[Lind]
+         })
+         )
+}
+
+## Create the call to model.frame from the matched call with the
+## appropriate substitutions and eliminations
+mkMFCall <- function(mc, form, nobars=FALSE) {
+    m <- match(c("data", "subset", "weights", "na.action", "offset"), names(mc), 0)
+    mc <- mc[c(1, m)]                   # retain only a subset of the arguments
+    mc$drop.unused.levels <- TRUE       # ensure unused levels of factors are dropped
+    mc[[1]] <- quote(model.frame)       # change the call to model.frame
+    form[[3]] <- if (nobars) {
+        if(is.null(nb <- nobars(form[[3]]))) 1 else nb
+    } else subbars(form[[3]])
+    mc$formula <- form
+    mc
+}
+
+## Create Lambdat as a ddiMatrix or a dgCMatrix and Zt as a dgCMatrix.
+## The list that is returned includes theta, lower, upper and thfun.
+mkLambdat <- function(grps, mms) {
+    ll <- list(Zt = do.call(rBind, mapply(Zsection, lapply(grps, as, Class="sparseMatrix"), mms)))
     nl <- sapply(grps, function(g) length(levels(g)))
-    zsl <- lapply(grps, as, Class="sparseMatrix")
-    mms <- lapply(rr, function(t) model.matrix(eval(substitute( ~ foo, list(foo = t[[2]]))), fr))
-    ll$Zt <- do.call(rBind, mapply(Zsection, zsl, mms))
     nc <- sapply(mms, ncol)
     if (all(nc == 1L)) {  # for scalar models use a diagonal Lambdat and a simpler thfun
-        nth <- length(zsl)
+        nth <- length(nc)
         ll$lower <- numeric(nth)
         ll$theta <- rep.int(1, nth)
         ll$upper <- rep.int(Inf, nth)
@@ -168,39 +203,4 @@ plsform <- function(formula, data, REML=TRUE, weights, offset, ...)  {
         })
     }
     ll
-}
-
-## Create a section of Zt from a sparse matrix of indicators, zt, and
-## a dense model matrix, mm When mm has a single column it is trivial
-## - multiply zt by a diagonal matrix For multiple columns, rBind the
-## products of zt and a diagonal matrix for each column, then
-## rearrange the order of the rows.
-Zsection <- function(zt,mm) {
-    if ((m <- ncol(mm)) == 1L) return(zt %*% Diagonal(x=mm))
-    ## Row indices.  If m = 2 and nrow(zt) = 10 we want the order 1,11,2,12,3,13,...,20
-    rinds <- as.vector(matrix(seq_len(m*nrow(zt)), nrow=m, byrow=TRUE))
-    do.call(rBind,lapply(seq_len(m), function(j) zt %*% Diagonal(x=mm[,j])))[rinds,]
-}
-
-## Create the diagonal block on Lambdat for a random-effects term with nc columns and nl
-## levels.  The value is a list with the starting value of theta for the block, the lower
-## bounds, the block of Lambdat and the function that updates the block given the section
-## of theta for this block.
-Lambdatblock <- function(nc, nl) {
-    if (nc == 1L)
-        return(list(theta = 1,
-                    lower = 0,
-                    Lambdat = Diagonal(x = rep(1, nl)),
-                    updateLambdatx=local({nl <- nl;function(theta) rep.int(theta[1],nl)})))
-    m <- diag(nrow=nc, ncol=nc); ut <- upper.tri(m, diag=TRUE)
-    theta <- m[ut]; m[ut] <- seq_along(theta)
-    Lambdat <- do.call(bdiag, lapply(seq_len(nl), function(i) m))
-    list(theta=theta,
-         lower=ifelse(theta,0,-Inf),
-         Lambdat=Lambdat,
-         updateLambdatx = local({
-             Lind <- Lambdat@x
-             function(theta) theta[Lind]
-         })
-         )
 }
